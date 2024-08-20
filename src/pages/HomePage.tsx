825cac34d80ebeecde2241be82c8b67c8893fdcc
dy.hwang <hwangdoyeonyu@gmail.com>
2024-08-20 17:12:40 +0900

    (add): homepage mbti section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,7 +22,7 @@ const HomePage: React.FC = () => {
         exit="out"
         variants={pageVariants}
         transition={pageTransition}
-        className="flex justify-between px-12 py-20"
+        className="flex flex-col justify-between px-12 py-20 space-y-12"
       >
         <motion.div
           initial={{ opacity: 0, x: -50 }}
@@ -38,6 +38,27 @@ const HomePage: React.FC = () => {
             and clinical trial data management. With expertise in Python,
             JavaScript, AWS, and more, I build scalable solutions for complex
             projects.
+          </p>
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.6, delay: 0.3 }}
+          className="max-w-md self-end"
+        >
+          <h2 className="text-3xl font-bold text-primary-600 dark:text-primary-400">
+            MBTI: INTP
+          </h2>
+          <h3 className="text-xl font-semibold mt-2 text-secondary-500 dark:text-secondary-400">
+            "The Contemplative Craftsman"
+          </h3>
+          <p className="mt-4 text-lg text-base dark:text-inverted">
+            As an INTP, I'm an innovative thinker with an insatiable thirst for
+            knowledge. I excel at solving complex problems through logical
+            reasoning and creative ideation. My ability to think outside the box
+            allows me to craft unique solutions and push the boundaries of
+            what's possible.
           </p>
         </motion.div>
       </motion.div>
